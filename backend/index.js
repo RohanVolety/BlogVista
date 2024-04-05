8135fb1e1adc841c36d9ce1cd15a6a531f944bca
Solaris <91217295+RohanVolety@users.noreply.github.com>
2024-04-05 18:58:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const connectDB = async () => {
 dotenv.config()
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname, "/images")))
-app.use(cors({ origin: "https://blogvista-psdd.onrender.com", credentials: true }))
+app.use(cors({ origin: "https://write-blogvista.netlify.app", credentials: true }))
 app.use(cookieParser())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
